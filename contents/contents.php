cf8f35e28c21d9fdfb0af2b6303a7bf702cbea13
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-18 16:04:54 +0300

    This is my first commit of the contents.php from our previous class

---
+++ contents/contents.php
@@ -0,0 +1,48 @@
+<?php
+
+class contents{
+    public function main_content(){
+    ?>
+
+    <div class="row">
+    <div class="content">
+        <h1>Main Content</h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi libero sunt doloremque qui necessitatibus saepe veniam impedit doloribus, ipsa itaque quia molestias aliquam. Minima earum ullam quod totam odio perspiciatis.
+    </p>
+
+    </div>
+
+    <?php
+    }
+
+
+    public function about_content(){
+        ?>
+    
+        <div class="row">
+        <div class="content">
+            <h1>About</h1>
+    
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi libero sunt doloremque qui necessitatibus saepe veniam impedit doloribus, ipsa itaque quia molestias aliquam. Minima earum ullam quod totam odio perspiciatis.
+        </p>
+    
+        </div>
+        <?php
+    }
+        public function side_bar(){
+        ?>
+        <div class="side_bar">
+            <h1>Side bar</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia nulla animi impedit amet ducimus consequatur aliquam est. Libero natus accusantium quasi voluptatibus commodi reiciendis, recusandae repellendus voluptates nesciunt sed debitis.
+            </p>
+
+        </div>
+
+    </div>
+        <?php
+    }
+}
