c5347cc9c983944c5bcb5536c73d09b7e1da6661
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-22 15:55:24 +0300

    Made a few changes to form.php

--- contents/form.php
+++ contents/form.php
@@ -4,34 +4,38 @@ class form{
         ?>
         <div class="container">
             <div class="row">
-        <div class="col-md-6" style="background-color:black; color:white;">
-        <form>
-        <fieldset>
+        <div class="col-md-6" >
+    
+        
 
         
         <legend>Disabled fieldset example</legend>
-        <div class="mb-3">
-            <label for="fullname" class="form-label">Full Name</label>
-            <input type="text" class="form-control form-control-lg" maxlegnth="50" id="fullname" aria-describedby="full name" placeholder="Your name">
-        
+        <div class="row align-items-md-stretch">
+        <div class="col-md-9">
+          <div class="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Sign Up</h2>
+            <form action="" method="POST" enctype="multipart/form-data">
+            <fieldset>    
+            <div class="mb-3">
+                    <label for="fullname" class="form-label">Fullname:</label>
+                    <input type="text" name="fullname" class="form-control form-control-lg" maxlength="50" id="fullname" placeholder="Enter your name">
+                </div>
+                <div class="mb-3">
+                    <label for="email_address" class="form-label">Email Address:</label>
+                    <input type="email" name="email_address" class="form-control form-control-lg" maxlength="50" id="email_address" placeholder="Enter your email address">
+                </div>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username:</label>
+                    <input type="text" name="username" class="form-control form-control-lg" maxlength="50" id="username" placeholder="Enter your username">
+                </div>
+                <button type="submit" name="signup" class="btn btn-primary">Submit</button>
+                </fieldset>  
+            </form>
+          </div>
         </div>
-   
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </fieldset>
-        </form>
+     
+       
+
         
         
            
