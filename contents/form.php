cdbe48b0d6b21d6fddfb27ab4877b74b77966c0f
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-23 20:08:07 +0300

    Adding an image to the sign up side to make it more interactive

--- contents/form.php
+++ contents/form.php
@@ -1,47 +1,50 @@
 <?php
-class form{
-    public function sign_up(){
+// Define a class named 'form'
+class form {
+    // Method to render the sign-up form
+    public function sign_up() {
         ?>
         <div class="container">
             <div class="row">
-        <div class="col-md-6" >
-    
-        
-
-        
-        <legend>Disabled fieldset example</legend>
-        <div class="row align-items-md-stretch">
-        <div class="col-md-9">
-          <div class="h-100 p-5 text-bg-dark rounded-3">
-            <h2>Sign Up</h2>
-            <form action="" method="POST" enctype="multipart/form-data">
-            <fieldset>    
-            <div class="mb-3">
-                    <label for="fullname" class="form-label">Fullname:</label>
-                    <input type="text" name="fullname" class="form-control form-control-lg" maxlength="50" id="fullname" placeholder="Enter your name">
+                <div class="col-md-6">
+                    <!-- Legend for the form -->
+                    <legend>Disabled fieldset example</legend>
+                    <div class="row align-items-md-stretch">
+                        <div class="col-md-9">
+                            <div class="h-100 p-5 text-bg-dark rounded-3">
+                                <h2>Sign Up</h2>
+                                <!-- Start of the sign-up form -->
+                                <form action="" method="POST" enctype="multipart/form-data">
+                                    <fieldset>    
+                                        <!-- Input for Fullname -->
+                                        <div class="mb-3">
+                                            <label for="fullname" class="form-label">Fullname:</label>
+                                            <input type="text" name="fullname" class="form-control form-control-lg" maxlength="50" id="fullname" placeholder="Enter your name">
+                                        </div>
+                                        <!-- Input for Email Address -->
+                                        <div class="mb-3">
+                                            <label for="email_address" class="form-label">Email Address:</label>
+                                            <input type="email" name="email_address" class="form-control form-control-lg" maxlength="50" id="email_address" placeholder="Enter your email address">
+                                        </div>
+                                        <!-- Input for Username -->
+                                        <div class="mb-3">
+                                            <label for="username" class="form-label">Username:</label>
+                                            <input type="text" name="username" class="form-control form-control-lg" maxlength="50" id="username" placeholder="Enter your username">
+                                        </div>
+                                        <!-- Submit button -->
+                                        <button type="submit" name="signup" class="btn btn-danger">Submit</button>
+                                    </fieldset>  
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="mb-3">
-                    <label for="email_address" class="form-label">Email Address:</label>
-                    <input type="email" name="email_address" class="form-control form-control-lg" maxlength="50" id="email_address" placeholder="Enter your email address">
+                <div class="col-md-6">
+                    <img src="img/sign_up_2.jpeg" alt="">
                 </div>
-                <div class="mb-3">
-                    <label for="username" class="form-label">Username:</label>
-                    <input type="text" name="username" class="form-control form-control-lg" maxlength="50" id="username" placeholder="Enter your username">
-                </div>
-                <button type="submit" name="signup" class="btn btn-danger">Submit</button>
-                </fieldset>  
-            </form>
-          </div>
+            </div>
         </div>
-     
-       
-
-        
-        
-           
-    </div>
-        </div>
-        </div>
+        <!-- Bootstrap JS for functionality -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>    
         <?php
     }
---
+++ layouts/menus.php
@@ -0,0 +1,29 @@
+<?php
+
+class menus{
+
+public function main_menu(){
+    ?>
+    <div class="topnav">
+        <a href= "./">Home</a>
+        <a href= "about.php">About Us</a>
+        <a href= "">Our Projects</a>
+        <a href= "">Our portfolio</a>
+        <a href= "">Contact us</a>
+        <?php $this->main_right_menu(); ?>
+
+</div>
+<?php
+}
+public function main_right_menu(){
+?>
+    <div class="topnav-right">
+        <a href= "./">Sign up</a>
+        <a href= "">Sign in</a>
+
+</div>
+
+<?php
+}
+
+}
