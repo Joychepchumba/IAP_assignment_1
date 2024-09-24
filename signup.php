8f0fc82400ea0c16b9c744734561f24f07f390c7
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-24 19:40:13 +0300

    Adding the signup.php file

---
+++ signup.php
@@ -0,0 +1,10 @@
+<?php
+ require_once "load.php";
+ $ObjLayouts->heading();
+ $ObjMenus->main_menu();
+ $ObjHeadings-> main_banner();
+$ObjForm->sign_up();
+$ObjCont->side_bar();
+$ObjLayouts->footer();
+
+?>
