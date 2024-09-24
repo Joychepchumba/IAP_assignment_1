d84d55b9f2b9775f753fc7094a702f5a00027240
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-24 19:38:28 +0300

    Adding the index.php files

---
+++ index.php
@@ -0,0 +1,18 @@
+
+
+    <?php
+    require_once "load.php";
+    $ObjLayouts->heading();
+    $ObjMenus->main_menu();
+    $ObjHeadings-> main_banner();
+    $ObjCont->about_content();
+    $ObjCont->side_bar();
+    $ObjLayouts->footer();
+    
+   
+    //print $obj->user_age("Tom",2004) <h1>This is my first page</h1>
+
+    ?>
+
+
+   
