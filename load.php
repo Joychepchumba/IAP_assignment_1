56125dc0478f575b6829dc7ffb21c520cdf0e8f2
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-24 19:38:56 +0300

    Adding the load.php file

---
+++ load.php
@@ -0,0 +1,49 @@
+<?php
+
+// Class Auto Load 
+
+function classAutoLoad($classname){
+
+    $directories = ["contents", "layouts", "menus","Processes"];
+
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
+    }
+}
+
+spl_autoload_register('classAutoLoad');
+
+// Create instances of all classes
+    $ObjLayouts = new layouts();
+    $ObjMenus = new menus();
+    $ObjHeadings = new headings();
+    $ObjCont = new contents();
+    $ObjForm= new form();
+    $ObjAuth = new auth();
+    
+
+require "includes/constants.php";
+require "includes/dbConnection.php";
+
+$conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+$ObjAuth->signup($conn);
+
+
+
+// print 
+// print "<br>";
+// print "<br>";
+// print $_SERVER["PHP_SELF"];
+// print "<br>";
+// print "<br>";
+// print basename($_SERVER["PHP_SELF"]);
+// print "<br>";
+// print "<br>";
+// if(file_exists("index.php") AND is_readable("index.php")){
+//     print "yes";
+// }else{
+//     print "no";
+// }
