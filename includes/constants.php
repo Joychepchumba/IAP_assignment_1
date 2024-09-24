6398f6f439713659af973dbcac62c66c5c54c3f1
Joychepchumba <joy.kipkemboi@strathmore.edu>
2024-09-24 19:44:13 +0300

    Adding the DB information for the connection

---
+++ includes/constants.php
@@ -0,0 +1,8 @@
+<?php
+    //Creating constants
+    define('DBTYPE', 'PDO');
+    define('HOSTNAME', '127.0.0.1');
+    define('DBPORT', '3307');
+    define('HOSTUSER', 'root');
+    define('HOSTPASS', 'Hospital19419.');
+    define('DBNAME', 'api_d');
